0f4afd4a4a9db93cc80e282198fcdf810eed9195
Ilya <sijonra@gmail.com>
2022-02-10 22:12:28 +0300

    Game field Added with Starter squareValue state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-class Test extends React.Component{
-  render(){
-    return(
-      <h1>
-        test
-      </h1>
-    )
+class Square extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      squareValue: null,
+    };
+  }
+
+  render() {
+    return (
+      <button 
+        onClick={()=>{this.setState({squareValue: 'X'})}} 
+        className="square"
+      >
+        {this.state.squareValue}
+      </button>
+    );
   }
 }
 
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square squareValue={i}/>;
+  }
+
+  render() {
+    
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <Test />,
+  <Game />,
   document.getElementById('root')
 );
